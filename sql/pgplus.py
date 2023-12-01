a5452c780028b8e4945d511acbe946594814bfae
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:21 +0900

    upload function

--- sql/pgplus.py
+++ sql/pgplus.py
@@ -1,28 +1,9 @@
 import pandas as pd
-
-
+from sqlalchemy.sql import text
 
 def read_from_server(schema_name:str,table_name:str,st_conn):
     with st_conn.connect() as conn_conn:
         return pd.read_sql_table(table_name=table_name,con=conn_conn,schema=schema_name)
-    
-def write_to_server(df:pd.DataFrame,schema_name:str,table_name:str,st_conn):
-    with st_conn.connect() as conn_conn:
-        return df.to_sql(name=table_name,con=conn_conn,schema=schema_name,if_exists='append',index=False)
-    
-def write_to_server_replace(df:pd.DataFrame,schema_name:str,table_name:str,st_conn):
-    with st_conn.session as session:
-        delete_sql = f'''
-            DELETE FROM {schema_name}.{table_name};
-        '''
-        try:
-          session.execute(delete_sql)
-          session.commit()
-        except:
-          pass #Nothing to delete
-
-    with st_conn.connect() as conn_conn:
-        return df.to_sql(name=table_name,con=conn_conn,schema=schema_name,if_exists='append')
 
 def get_foreign_keys(schema:str,table:str,conn):
     foreign_key_sql = f'''
@@ -72,3 +53,20 @@ def get_default_value(schema:str,table:str,conn):
         ret = pd.read_sql_query(sql,con=con_con)
     
         return ret.set_index('column_name')
+    
+
+      
+def write_to_server(df:pd.DataFrame,schema_name:str,table_name:str,st_conn):
+    with st_conn.connect() as conn_conn:
+        return df.to_sql(name=table_name,con=conn_conn,schema=schema_name,if_exists='append',index=False)
+
+def upload_to_sql_by_value(schema_name,table_name,select_row_by_column,select_row_by_column_val,column_name,value_name,st_conn):
+    update_query = text(f"""
+    UPDATE {schema_name}.{table_name}
+    SET {column_name} = {value_name}
+    WHERE {select_row_by_column} = '{select_row_by_column_val}';
+    """)
+    update_query
+    with st_conn.session as session:
+        session.execute(update_query)
+        session.commit()
