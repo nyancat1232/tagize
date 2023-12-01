406ae46f394ab6b9bb67af77018aa47db6a44ccd
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    add functions

--- builtin_library.py
+++ builtin_library.py
@@ -18,3 +18,22 @@ def date_only_month_and_day(str_date,spearator='.'):
     if comp_date > now_date:
         year -= 1
     return datetime(year,month,day)
+
+def rename_duplicate(column_names):
+    def check_duplicate(temp_buffer,s,index):
+        if s in temp_buffer:
+            return s+str(index)
+        temp_buffer.append(s)
+        return ""
+
+    temp_buffer=[]
+    return [f"{s}{check_duplicate(temp_buffer,s,i)}"for i,s in enumerate(column_names)]
+
+def divide_by_column(list_lines,row_num=1,sep='\t'):
+    new_list = []
+    for i in range(0,len(list_lines),row_num):
+        concat_line = []
+        for ii in range(row_num):
+            concat_line.append(list_lines[i+ii])
+        new_list.append(sep.join(concat_line).split(sep))
+    return new_list
