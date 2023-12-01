8798f1da053e9170cc2406c03f2fd66250adc1c9
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    Fix PDLibrary

--- pandas_library.py
+++ pandas_library.py
@@ -8,18 +8,17 @@ class PDLibrary:
     _data = pd.DataFrame()
     _path = ''
 
-    def __init__(self,path,*columns_name,index_col='',init_val=np.nan):
+    def __init__(self,path,*columns_name,index_name='',init_val=np.nan):
         self._path = path
         try:
-            self._data = pd.read_csv(self._path,index_col=index_col,usecols=columns_name)
+            self._data = pd.read_csv(self._path,index_col=index_name)
         except FileNotFoundError:
             self._data = pd.DataFrame(columns=columns_name)
-            self._data.set_index(index_col)
-            self._data.to_csv(path_or_buf = self._path)
+            self._data.index.name = index_name
+            self.save_data()
 
-    def append_data(self,new_index=None,init_var=np.nan,**col_val):
-        if new_index is None:
-            new_index = datetime.utcnow().strftime("%F %T")
+    def append_data_nowtime(self,init_var=np.nan,**col_val):
+        new_index = datetime.utcnow().strftime("%F %T")
         
         self._data.loc[new_index] = np.nan
         for column_name,column_val in col_val.items():
@@ -36,13 +35,8 @@ class PDLibrary_Streamlit(PDLibrary):
         for attr_name in self._data.columns:
             st.selectbox(label=attr_name,options=self._data[attr_name].unique())
 
-    def add_save_download_button(self,label):
+    def add_save_download_button(self,label="Download it"):
         st.download_button(label=label,data=self._data.to_csv().encode('UTF-8'),file_name='result.csv',)
-    
-    def append_and_save_data(self,label,**column_data):
-        if st.button(label=label):
-            self.append_data(**column_data)
-            self.save_data()
     
     def show_data(self):
         st.dataframe(self._data)
