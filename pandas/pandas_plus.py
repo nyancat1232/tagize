f971b8e537379e0258c5172bfec768f9db4a4e86
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:21 +0900

    for safety

--- pandas/pandas_plus.py
+++ pandas/pandas_plus.py
@@ -2,4 +2,4 @@ import pandas as pd
 from typing import Optional
 
 def create_new_dataframe_keep_columns(df:pd.DataFrame,index:Optional[pd.Series] = None)->pd.DataFrame:
-    return pd.DataFrame(columns=df.columns,index=index).astype(dtype=df.dtypes)
+    return pd.DataFrame(columns=df.columns,index=index).astype(dtype=df.dtypes).copy()
