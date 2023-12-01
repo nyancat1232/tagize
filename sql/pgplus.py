539a0cdbfff9615b0f5b244dac5f97a8d4900ae7
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:21 +0900

    unify

--- sql/pgplus.py
+++ sql/pgplus.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from sqlalchemy.sql import text
+'''
+DO NOT USE AS PUBLIC. 
+'''
 
 def read_from_server(schema_name:str,table_name:str,st_conn):
     with st_conn.connect() as conn_conn:
         return pd.read_sql_table(table_name=table_name,con=conn_conn,schema=schema_name)
 
-def get_foreign_keys(schema:str,table:str,conn):
+def get_foreign_keys(schema_name:str,table_name:str,st_conn):
     foreign_key_sql = f'''
     SELECT KCU.column_name AS current_column_name,
         CCU.table_schema AS upper_schema, 
@@ -15,41 +18,41 @@ def get_foreign_keys(schema:str,table:str,conn):
     JOIN information_schema.constraint_column_usage AS CCU ON KCU.constraint_name = CCU.constraint_name
     JOIN information_schema.table_constraints AS TC ON KCU.constraint_name = TC.constraint_name
     WHERE TC.constraint_type = 'FOREIGN KEY'
-    AND KCU.table_schema='{schema}'
-    AND KCU.table_name='{table}';
+    AND KCU.table_schema='{schema_name}'
+    AND KCU.table_name='{table_name}';
     '''
 
-    with conn.connect() as con_con:
+    with st_conn.connect() as con_con:
         ret = pd.read_sql_query(foreign_key_sql,con=con_con).drop_duplicates()
     
         return ret.set_index('current_column_name')
     
-def get_identity(schema:str,table:str,conn):
+def get_identity(schema_name:str,table_name:str,st_conn):
     sql = f'''SELECT attname as identity_column
   FROM pg_attribute 
   JOIN pg_class 
     ON pg_attribute.attrelid = pg_class.oid
   JOIN pg_namespace
     ON pg_class.relnamespace = pg_namespace.oid
- WHERE nspname = '{schema}'
-   AND relname = '{table}'
+ WHERE nspname = '{schema_name}'
+   AND relname = '{table_name}'
    AND attidentity = 'a';
 '''
     
-    with conn.connect() as con_con:
+    with st_conn.connect() as con_con:
         ret = pd.read_sql_query(sql,con=con_con)
     
         return ret['identity_column']
     
-def get_default_value(schema:str,table:str,conn):
+def get_default_value(schema_name:str,table_name:str,st_conn):
     sql = f'''SELECT column_name, column_default
   FROM information_schema.columns
- WHERE table_schema = '{schema}'
-   AND table_name = '{table}'
+ WHERE table_schema = '{schema_name}'
+   AND table_name = '{table_name}'
    AND column_default IS NOT NULL ;
 '''
         
-    with conn.connect() as con_con:
+    with st_conn.connect() as con_con:
         ret = pd.read_sql_query(sql,con=con_con)
     
         return ret.set_index('column_name')
