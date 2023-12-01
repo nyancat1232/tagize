955b925487110e1e4ec84313b426d26ab10bd72c
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    Remove unnecessary functions

--- convert.py
+++ convert.py
@@ -1,10 +1,3 @@
-def convert_line_file(input_file,out_file):
-    with open(out_file,mode='w',encoding='UTF-8') as ouf:
-        with open(input_file,mode='r',encoding='UTF-8') as inf:
-            while line := inf.readline() :
-                rrr = line.split('\t')
-                ouf.writelines(rrr[1]+'\n')
-
 def check_if_str(function):
     def new_func(input_str):
         if type(input_str) != str:
