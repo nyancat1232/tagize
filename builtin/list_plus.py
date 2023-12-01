0a4e00a5e076b25c6206bfc9f827933a0a442f78
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:21 +0900

    module

--- builtin/list_plus.py
+++ builtin/list_plus.py
@@ -18,3 +18,13 @@ def list_change_position(list_,index_from,index_to,mutable=False):
         ret = temp[:index_from]+temp[index_from+1:]
         ret.insert(index_to,temp_val)
         return ret
+    
+def split_list(list_in, split_col_num=1):
+    return [list_in[col::split_col_num] for col in range(split_col_num)]
+
+def transpose_list(list_in,default_val=None):
+    list_temp = list_in.copy()
+    if len(list_temp[-1])-len(list_temp[0]) != 0:
+        for l in list_temp:
+            l.append(default_val)
+    return list(map(list, zip(*list_temp)))
--- builtin_plus.py
+++ builtin_plus.py
@@ -1,14 +1,5 @@
 from datetime import datetime
 
-def split_list(list_in, split_col_num=1):
-    return [list_in[col::split_col_num] for col in range(split_col_num)]
-
-def transpose_list(list_in,default_val=None):
-    list_temp = list_in.copy()
-    if len(list_temp[-1])-len(list_temp[0]) != 0:
-        for l in list_temp:
-            l.append(default_val)
-    return list(map(list, zip(*list_temp)))
 
 def date_only_month_and_day(str_date,spearator='.'):
     month,day = map(int,str_date.split(spearator))
