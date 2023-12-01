eaa5cc3763de9dc0aa3e0207a6f6b83caa2fd779
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    add functions

--- builtin_library.py
+++ builtin_library.py
@@ -37,3 +37,20 @@ def divide_by_column(list_lines,row_num=1,sep='\t'):
             concat_line.append(list_lines[i+ii])
         new_list.append(sep.join(concat_line).split(sep))
     return new_list
+
+
+
+def filter_by_exact_length(words,query):
+    return [word for word in words if len(word)==len(query)]
+
+def count_trues(li):
+    return len([element for element in li if element==True])
+               
+def check_match(word,query):
+    if len(word)!=len(query):
+        raise IndexError("Not exact length")
+    
+    results = [any([char1==char2,char2=="?"]) for char1,char2 in zip(word,query) ]
+    count = (count_trues(results)==len(word))
+    return {'count':count,'results':results}
+
