48abf6a234a8cf8c5cf10d069898481c5631deed
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    suppor any input

--- convert.py
+++ convert.py
@@ -1,15 +1,15 @@
 def check_if_str(function):
-    def new_func(input_str):
+    def new_func(input_str,**kwarg):
         if type(input_str) != str:
-            raise TypeError
-        return function(input_str)
+            raise TypeError("Must be str")
+        return function(input_str,**kwarg)
     return new_func
 
 #(check_if_str(convert_line_str))(input_str)
 @check_if_str
-def convert_line_str(input_str):
+def convert_line_str(input_str,include_in_line = ''):
     def processing_line(line):
-        if line.find('In [') != -1:
+        if line.find(include_in_line) != -1:
             return line[line.find(': ')+len(': '):]
         else:
             return None
