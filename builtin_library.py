558f46b6497752fa0601e360714188f51c0b0a88
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:20 +0900

    add function

--- builtin_library.py
+++ builtin_library.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def split_list(list_in, split_col_num=1):
     return [list_in[col::split_col_num] for col in range(split_col_num)]
 
@@ -7,3 +9,12 @@ def transpose_list(list_in,default_val=None):
         for l in list_temp:
             l.append(default_val)
     return list(map(list, zip(*list_temp)))
+
+def date_only_month_and_day(str_date,spearator='.'):
+    month,day = map(int,str_date.split(spearator))
+    now_date = datetime.utcnow()
+    year = now_date.year
+    comp_date = datetime(year,month,day)
+    if comp_date > now_date:
+        year -= 1
+    return datetime(year,month,day)
