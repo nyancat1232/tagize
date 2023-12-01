98596df9c5b921c041e81893bd2723e7d09f7969
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:30:21 +0900

    init

---
+++ sql/sqlite3_plus.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+def _db_query_exec(sql:str,db:str,show:bool=False):
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+
+    rr = cur.execute(sql)
+    if show:
+        for r in rr:
+            print(r)
+    con.commit()
+    con.close()
+
+def create(table,**columns):
+
+    sql = f'''CREATE TABLE IF NOT EXISTS {table}(
+                {','.join(' '.join([key,val]) for key,val in columns.items())}
+    );
+                '''
+    _db_query_exec(sql)
+
+def read(table,*columns):
+    sql = f"SELECT {','.join(columns)} FROM {table}"
+    _db_query_exec(sql,True)
+
+def update(table,**adds):
+    keys = ['"'+c+'"' for c in adds.keys()]
+    values = ['"'+c+'"' for c in adds.values()]
+    sql = f"INSERT INTO {table} ({','.join(keys)}) VALUES({','.join(values)})"
+    print(sql)
+    _db_query_exec(sql)
+
+def delete(table):
+    sql = f'''DROP TABLE {table}
+    '''
+    _db_query_exec(sql)
