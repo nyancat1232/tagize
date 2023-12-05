0acdebdfab850b5b021c7b2477f806f216bb0498
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-05 19:06:37 +0900

    perceptron making

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -12,19 +12,35 @@ class Node:
     outs_backward : float = 0.0
 
 
-    def connect_ins(self,elem):
-        self.ins_forward.append(elem)
+    def connect_ins(self,node):
+        self.ins_forward.append(node)
         self.ins_backward.append(0.0)
     
     def forward(self):
+        for in_forward in self.ins_forward:
+            try:
+                print(in_forward)
+                in_forward.forward()
+            except:
+                continue
+
+        temp=[]
+        
         self.outs_forward = self._forw_func(self.ins_forward)
         if self.is_terminal:
             self.outs_backward = self.outs_forward
-        return self.outs_forward
+        return self
     
     def backward(self):
         self.ins_backward = [self._back_func(self.outs_backward,inp) for inp in self.ins_forward]
-        return self.ins_backward
+        return self
+    
+    def summary(self):
+        return f'''\n
+        {"forward_line":_>20.20} {"backward_line":_>20.20}\n
+        {str(self.ins_forward):_>20.20} {str(self.ins_backward):_>20.20}\n
+        {str(self.outs_forward):_>20.20} {str(self.outs_backward):_>20.20}\n
+        '''
 
 
 class Add(Node):
@@ -63,3 +79,11 @@ class Mult(Node):
         def _back_func(out_back,in_forw):
             raise "ooo"
         self._back_func = _back_func
+
+
+# y=ax+b, x=1,y=3
+# a<-random
+# b<-random
+# layer1=Mult([a,x])
+# layer2=Add([layer1,b])
+# layer_error=Add([layer2,-y],terminal=True)
