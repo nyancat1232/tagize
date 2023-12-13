93db153a720be7d62658eefa3e05ea1916665e98
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 16:45:23 +0900

    aaa

--- pytorch/simple.py
+++ pytorch/simple.py
@@ -2,26 +2,32 @@ import torch
 from dataclasses import dataclass,field
 
 from typing import Any,List
-
+#https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 @dataclass
 class TorchPlus:
     meta_optimizer : torch.optim.Optimizer = None
+    meta_optimizer_learning_rate : float = 0.01
+    meta_optimizer_epoch : int = 2000
     meta_error_measurement : Any = None
     meta_activator : Any = None
-    model : torch.nn.Module = None
-    model_input_datas : Any = None
-    model_input_labels : Any = None
+    constatns : Any
+    parameters : List[Any] = None
+    reflect_predictions : Any
+    reflect_labels : Any
 
-    def init_metas(self):
+    def calc(self):
         loss_fn = self.meta_error_measurement()
-        mo_instance = self.model()
-        optimizer = self.meta_optimizer(mo_instance.parameters(), lr=1e-3)
+        optimizer = self.meta_optimizer(self.model_parameters, lr=self.meta_optimizer_learning_rate)
+        for t in range(self.meta_optimizer_epoch):
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(x)
 
-        
-        return TorchPlusMetaInstance(loss=loss_fn,model=mo_instance,optimizer=optimizer)
-    
-@dataclass
-class TorchPlusMetaInstance:
-    loss : Any
-    model: Any
-    optimizer: Any
+            # Compute and print loss
+            loss = loss_fn(y_pred, y)
+            if t % 100 == 99:
+                print(t, loss.item())
+
+            # Zero gradients, perform a backward pass, and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
