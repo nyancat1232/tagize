04933251d1264be4e7af73c7e7a73e2748049a25
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 21:19:21 +0900

    add process

--- pytorch/simple.py
+++ pytorch/simple.py
@@ -1,7 +1,7 @@
 import torch
 from dataclasses import dataclass,field
 
-from typing import Any,List,Dict
+from typing import Any,Dict,Callable
 #https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 @dataclass
 class TorchPlus:
@@ -13,6 +13,9 @@ class TorchPlus:
     
     all_tensors : Dict = field(default_factory=dict)
 
+    assign_process_values : Callable = None
+    assign_process_process : Callable = None
+
     def get_all_params(self):
         return [self.all_tensors[key] for key in self.all_tensors if self.all_tensors[key].requires_grad]
     def train_one_step_by_equation(self,label,prediction_quation):
@@ -21,3 +24,14 @@ class TorchPlus:
         optim.zero_grad()
         loss.backward()
         optim.step()
+    def train(self):
+        #all terminals
+        self.assign_process_values(self)
+        
+        for _ in range(self.meta_optimizer_epoch):
+            #process
+            self.assign_process_process(self)
+
+            #train
+            self.train_one_step_by_equation(self._label,self._pred)
+        return self.get_all_params()
