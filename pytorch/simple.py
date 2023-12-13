5bb3b3d0bcfe4812094300b76c68930c4de3f5fe
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 18:39:17 +0900

    simplify

--- pytorch/simple.py
+++ pytorch/simple.py
@@ -12,19 +12,3 @@ class TorchPlus:
     meta_activator : Any = None
     
     all_tensors : Dict = field(default_factory=dict)
-
-    def compile_model_block(self,str):
-        raise "No implementation"
-
-    def gen_all_params(self):
-        self.all_params=[self.all_tensors[key] for key in self.all_tensors if self.all_tensors[key].requires_grad]
-        return self.all_params
-    
-    def train(self):
-        for _ in range(self.meta_optimizer_epoch):
-            loss = self.meta_error_measurement()(self.all_tensors['_lval'], self._rval)
-            optim = self.meta_optimizer(self.all_params,lr=0.1)
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-        return self.all_params
