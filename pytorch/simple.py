4c77e27e0d98d9ab3f60c582c1de77ad03e485f1
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-14 18:37:32 +0900

    default meta parameter

--- pytorch/simple.py
+++ pytorch/simple.py
@@ -5,10 +5,10 @@ from typing import Any,Dict,Callable
 #https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 @dataclass
 class TorchPlus:
-    meta_optimizer : torch.optim.Optimizer = None
-    meta_optimizer_learning_rate : float = 0.01
+    meta_optimizer : torch.optim.Optimizer = torch.optim.SGD
+    meta_optimizer_learning_rate : float = 0.015
     meta_optimizer_epoch : int = 2000
-    meta_error_measurement : Any = None
+    meta_error_measurement : Any = torch.nn.MSELoss
     meta_activator : Any = None
     
     all_leaf_tensors : Dict[str,torch.Tensor] = field(default_factory=dict)
