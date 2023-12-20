cba2d766239a2adb0fd116c78bb3f3f5a8e6cd8a
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-20 12:25:27 +0900

    trying to remove TensorSequence._tensor_name

--- pytorch/internal/internal.py
+++ pytorch/internal/internal.py
@@ -70,31 +70,29 @@ class TensorsSquence:
     _tensors : List[TorchTensorPlusInternal] = field(repr=False,init=False)
 
     def __post_init__(self):
-        self._tensor_name = []
         self._tensors = []
 
     def __getitem__(self,sequence_ind) ->Dict[str,TorchTensorPlusInternalSequenced]:
-        return {self._tensor_name[index] : self._tensors[index][sequence_ind] for index,_ in enumerate(self._tensor_name)}
+        return [self._tensors[index][sequence_ind] for index,_ in enumerate(self._tensors)]
     
     def new_tensor(self,tensorplus:TorchTensorPlusInternal,tensor:torch.Tensor):
         current_ttp = tensorplus
         current_ttp.tensor = tensor
         self._tensors.append(current_ttp) 
-        self._tensor_name.append(current_ttp.name)
 
     def change_tensor(self,name,tensor:torch.Tensor):
-        for current_name_index,current_name in enumerate(self._tensor_name):
+        for current_name_index,current_name in enumerate([tensor.name for tensor in self._tensors]):
             if current_name == name:
                 self._tensors[current_name_index].tensor = tensor
     
     def get_all_params(self):
-        return {name:tensor.tensor for name,tensor in zip(self._tensor_name,self._tensors) if tensor.ttype == TTPType.PARAMETER}
+        return {tensor.name :tensor.tensor for tensor in self._tensors if tensor.ttype == TTPType.PARAMETER}
 
 
 def unsqueeze_tensors(tensors:Dict[str,TorchTensorPlusInternalSequenced],max_dim=None):
     if max_dim is None:
-        max_dim = max([tensors[key].tensor.dim() for key in tensors])
+        max_dim = max([tensors[key].tensor.dim() for key,_ in enumerate(tensors)])
 
-    return {key : tensors[key].unsqueeze_to(max_dim) for key in tensors},max_dim
+    return {tensors[key].name : tensors[key].unsqueeze_to(max_dim) for key,_ in enumerate(tensors)},max_dim
 
         
