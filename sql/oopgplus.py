dbe4d89547ba136c4104ed92e9518a29ce7dbc87
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-06 20:14:37 +0900

    add init of TableStructure

--- sql/oopgplus.py
+++ sql/oopgplus.py
@@ -7,6 +7,13 @@ from typing import List
 class TableStructure:
     schema_name : str
     table_name : str
+    engine : sqlalchemy.Engine
+
+    def __init__(self,schema_name:str,table_name:str,engine:sqlalchemy.Engine):
+        self.schema_name = schema_name
+        self.table_name = table_name
+        self.engine = engine
+
 
 class SQLALchemyPlus:
     engine : sqlalchemy.Engine
