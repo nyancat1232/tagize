0fa0343ea6d4a7cfa85c5bb8b472a558d187caa5
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-06 19:53:10 +0900

    init

---
+++ sql/oopgplus.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy.sql import text
+from dataclasses import dataclass
+import sqlalchemy
+
+@dataclass
+class TableStructure:
+    schema_name : str
+    table_name : str
+
+class SQLALchemyPlus:
+    engine : sqlalchemy.Engine
+
+    def __init__(self,engine:sqlalchemy.Engine):
+        self.engine = engine
