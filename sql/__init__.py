456d2b1192f5b5ac2efecfeef3ac5dd62ee1da07
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-07 22:42:06 +0900

    separate legacy

--- sql/__init__.py
+++ sql/__init__.py
@@ -1,10 +1,13 @@
-from pyplus.sql.pgplus import read_from_server
-from pyplus.sql.pgplus import expand_foreign_column
-from pyplus.sql.pgplus import get_identity
-from pyplus.sql.pgplus import get_columns
-
-from pyplus.sql.pgplus import upload_to_sql_by_id
-from pyplus.sql.pgplus import write_to_server
-from pyplus.sql.pgplus import create_empty_with_id_with_column
-
 from pyplus.sql.oopgplus import TableStructure
+from pyplus.sql.oopgplus import get_table_list
+
+compat = False
+if compat:
+    from pyplus.sql.pgplus import read_from_server
+    from pyplus.sql.pgplus import expand_foreign_column
+    from pyplus.sql.pgplus import get_identity
+    from pyplus.sql.pgplus import get_columns
+
+    from pyplus.sql.pgplus import upload_to_sql_by_id
+    from pyplus.sql.pgplus import write_to_server
+    from pyplus.sql.pgplus import create_empty_with_id_with_column
