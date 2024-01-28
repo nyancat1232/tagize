8b080786849a8918dae8ec4f89b3f21999dc582b
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-28 16:54:52 +0900

    add function

--- pandas/pandas_plus.py
+++ pandas/pandas_plus.py
@@ -3,3 +3,7 @@ from typing import Optional
 
 def create_new_dataframe_keep_columns(df:pd.DataFrame,index:Optional[pd.Series] = None)->pd.DataFrame:
     return pd.DataFrame(columns=df.columns,index=index).astype(dtype=df.dtypes).copy()
+
+def to_date(sr:pd.Series)->pd.Series:
+    return sr.dt.date
+
