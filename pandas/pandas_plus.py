87c334cb0393dcdd400a51ebc0f0792db597380e
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-28 17:34:22 +0900

    annotation

--- pandas/pandas_plus.py
+++ pandas/pandas_plus.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import Optional,Callable,Tuple
+from typing import Optional,Callable,Tuple,List
 
 def create_new_dataframe_keep_columns(df:pd.DataFrame,index:Optional[pd.Series] = None)->pd.DataFrame:
     return pd.DataFrame(columns=df.columns,index=index).astype(dtype=df.dtypes).copy()
@@ -7,6 +7,6 @@ def create_new_dataframe_keep_columns(df:pd.DataFrame,index:Optional[pd.Series] 
 def to_date(sr:pd.Series)->pd.Series:
     return sr.dt.date
 
-def horizontal_applier(df:pd.DataFrame,func:Callable,*cols:Tuple[str]):
+def horizontal_applier(df:pd.DataFrame,func:Callable[[Tuple[pd.Series]],pd.Series],*cols:Tuple[str]):
     l_cols = list(cols)
     return df[l_cols].apply(lambda c : func(*c),axis=1)
