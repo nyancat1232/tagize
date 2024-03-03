fb7848e6a938283802d276976d0214369ece810f
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-04 01:52:49 +0900

    add drop method

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ df_meta
 
 cols = st.multiselect('select columns',df_meta.index)
 
-def tableize(df_content:pd.DataFrame,df_meta:pd.DataFrame,selected_cols:list[str],tag_column_name:str='tag')->pd.DataFrame:
+def tableize(df_content:pd.DataFrame,df_meta:pd.DataFrame,selected_cols:list[str],tag_column_name:str='tag',drop_if:str|None=None)->pd.DataFrame:
     df_content = df_content.copy()
     df_meta = df_meta.copy()
 
@@ -23,10 +23,17 @@ def tableize(df_content:pd.DataFrame,df_meta:pd.DataFrame,selected_cols:list[str
 
     for col in selected_cols:
         def pr(strs:list[str]):
-            return [s for s in strs if s in meta_hashtag_split[col]]
+            rrr = [s for s in strs if s in meta_hashtag_split[col]]
+            if len(rrr)>0:
+                return [s for s in strs if s in meta_hashtag_split[col]]
+            else:
+                return None
         df_content[col]=content_hashtag_split.apply(pr)
+
+    if drop_if:
+        df_content = df_content.dropna(axis=0,subset=selected_cols,how=drop_if)
 
     return df_content
 
-df_res = tableize(df_content=df_content,df_meta=df_meta,selected_cols=cols)
+df_res = tableize(df_content=df_content,df_meta=df_meta,selected_cols=cols,drop_if='any')
 df_res
