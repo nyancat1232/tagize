346ed2bfdf0d0bbcf42dbb001ff2d9de0084dc1f
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-11 00:44:11 +0900

    prune

--- main.py
+++ main.py
@@ -57,30 +57,3 @@ for v in iter_split_supertag(df_content):
     for ind,col in enumerate(cols):
         with col:
             v[ind]
-st.stop()
-
-cols = st.multiselect('select columns',df_group.index)
-
-def tableize(df_content:pd.DataFrame,df_meta:pd.DataFrame,selected_cols:list[str],tag_column_name:str='tag',drop_if:str|None=None)->pd.DataFrame:
-    df_content = df_content.copy()
-    df_meta = df_meta.copy()
-
-    content_hashtag_split=split_hashtag(df_content[tag_column_name])
-    meta_hashtag_split=split_hashtag(df_meta[tag_column_name])
-
-    for col in selected_cols:
-        def pr(strs:list[str]):
-            rrr = [s for s in strs if s in meta_hashtag_split[col]]
-            if len(rrr)>0:
-                return [s for s in strs if s in meta_hashtag_split[col]]
-            else:
-                return None
-        df_content[col]=content_hashtag_split.apply(pr)
-
-    if drop_if:
-        df_content = df_content.dropna(axis=0,subset=selected_cols,how=drop_if)
-
-    return df_content
-
-df_res = tableize(df_content=df_content,df_meta=df_group,selected_cols=cols,drop_if='any')
-df_res
