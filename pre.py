89c69a73c9d44166ba4b83b883ecc24dc78260a6
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-18 14:45:39 +0900

    remove for less dependancy

--- pre.py
+++ pre.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pyplus.streamlit as stp
 
-def ex():
-    if not stp.check_password():
-        st.stop()
 if 'conn' not in st.session_state:
     st.session_state['conn'] = st.connection('myaddress',type='sql')
 
