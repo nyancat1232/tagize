c96b67eff47de29314f0c41e862dde8f8e7d791a
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-24 17:30:28 +0900

    compare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pyplus.sql as sqlp
+import pyplus.streamlit as stp
 
 from pre import conn
 
@@ -10,12 +11,16 @@ from hashprocessor.process import split_hashtag
 ts = sqlp.TableStructure(schema_name=st.secrets['address_007']['schema'],table_name=st.secrets['address_007']['table'],engine=conn.engine)
 df = ts.read()
 
+
 df_edit = df.copy()
 df_edit['tags']=df_edit['tags'].apply(lambda li:"#".join(['']+li))
-df_edit = st.data_editor(df_edit,num_rows='dynamic')
+df_edit = st.data_editor(df_edit,disabled=ts.refresh_identity())
 
-df_post = df_edit.copy()
-df_post['tags'] = split_hashtag(df_edit['tags'])
-df_post
+df_post = df.copy()
+
+df_post_edit = df_edit.copy()
+df_post_edit['tags'] = split_hashtag(df_edit['tags'])
+
+stp.write_columns(original=df_post,edit=df_post_edit)
 
 st.stop()
