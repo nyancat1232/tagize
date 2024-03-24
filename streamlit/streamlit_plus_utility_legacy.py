1e590e6fc6b2ed9b8f9315c08a0307b2c69ab034
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-24 17:30:15 +0900

    remove dependency

--- streamlit/streamlit_plus_utility_legacy.py
+++ streamlit/streamlit_plus_utility_legacy.py
@@ -1,9 +1,59 @@
 import streamlit as st
 import pandas as pd
+import pdfplumber as pdfp
 from dataclasses import dataclass
 from typing import Callable,Any
 import re
 from unicodedata import normalize
+
+def from_csv_to_dataframe(label:str,**dataframe_keywords)->pd.DataFrame:
+    '''
+    Read a csv file using a ßstrealit uploader
+    ## Parameters:
+    label : str
+        A short label explaining to the user what this file uploader is for. The label can optionally contain Markdown and supports the following (from streamlit==1.28.1 docstring)
+    dataframe_keywords : keywords of pd.Dataframe
+        keyword arguments for initializing pd.Dataframe
+    ## See Also:
+    from_pdf_to_dataframe
+    from_txt_to_dataframe
+    from_xls_to_dataframe
+    from_parquet_to_dataframe
+
+    '''
+
+    if file := st.file_uploader(label=label,type="csv"):
+        return pd.read_csv(filepath_or_buffer=file,**dataframe_keywords)
+
+
+def from_pdf_to_dataframe(label,number=0,**dataframe_keywords)->pd.DataFrame:
+    if file := st.file_uploader(label=label,type="pdf"):
+        #df = tb.read_pdf(file)[number]
+        with pdfp.open(file) as pdf:
+            lis_ = pdf.pages[0].extract_table()
+        return pd.DataFrame(lis_,**dataframe_keywords)
+
+def from_txt_to_dataframe(label,preprocess_function,**dataframe_keywords)->pd.DataFrame:
+    if text := st.text_area(label=label):
+        txt_pre = preprocess_function(text)
+        df = pd.DataFrame(txt_pre,**dataframe_keywords)
+        return df.set_axis(df.loc[0],axis=1).drop(labels=[0],axis=0)
+
+def from_xls_to_dataframe(label,**dataframe_keywords)->pd.DataFrame:
+    if file := st.file_uploader(label=label,type='xls'):
+        return pd.read_excel(file,engine="xlrd",**dataframe_keywords)
+    
+def from_parquet_to_dataframe(label,**dataframe_keywords)->pd.DataFrame:
+    """Read a parquet file using a ßstrealit uploader
+
+    Args:
+        label (str): texts to show.
+
+    Returns:
+        DataFrame: A dataframe
+    """
+    if file := st.file_uploader(label=label,type="parquet"):
+        return pd.read_parquet(path=file,**dataframe_keywords)
 
 @dataclass
 class FileDescription:
