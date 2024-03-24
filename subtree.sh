9fa37b8053c7570790b5db07d34bd86e576a8e1e
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-24 13:15:46 +0900

    mode selection

--- subtree.sh
+++ subtree.sh
@@ -1,1 +1,10 @@
-git subtree pull --prefix=pyplus pyplus_origin master
+
+read -p "Select a mode(L: puLl, S:puSh)" MODE
+
+if [ "$MODE" == "L" ]; then
+    git subtree pull --prefix=pyplus pyplus_origin master
+fi
+
+if [ "$MODE" == "S" ]; then
+    git subtree push --prefix=pyplus pyplus_origin master
+fi
