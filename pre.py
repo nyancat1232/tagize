56f0e4de79ecc36fa6d841b3ffa7bb81fdf4d4ba
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-25 12:04:42 +0900

    global

--- pre.py
+++ pre.py
@@ -10,3 +10,10 @@ if 'conn' not in st.session_state:
     st.session_state['conn'] = st.connection('myaddress',type='sql')
 
 conn = st.session_state['conn']
+
+import pyplus.sql as sqlp
+
+if 'ts' not in st.session_state:
+    st.session_state['ts'] = sqlp.TableStructure(schema_name=st.secrets['address_007']['schema'],table_name=st.secrets['address_007']['table'],engine=conn.engine)
+
+ts:sqlp.TableStructure = st.session_state['ts']
