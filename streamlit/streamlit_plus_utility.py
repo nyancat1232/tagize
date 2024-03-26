2a2d6e7690be5acafa2e239131114c30cc624e58
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-26 10:37:11 +0900

    new way to use this library

--- streamlit/streamlit_plus_utility.py
+++ streamlit/streamlit_plus_utility.py
@@ -8,17 +8,7 @@ from unicodedata import normalize
 @dataclass
 class FileDescription:
     '''
-    methods how to read a file
-    ## See Also:
-    execute_file_descriptions
-    ## Examples:
-    >>> import pandas as pd
-    >>> from pyplus.streamlit.streamlit_plus_utility import FileDescription,execute_file_descriptions
-    >>> 
-    >>> fds : list[FileDescription]=[]
-    >>> fds.append(FileDescription('^train.csv$',pd.read_csv))
-    >>> fds.append(FileDescription('^codebook.csv$',pd.read_csv))
-    >>> dfs = execute_file_descriptions(fds)
+    Warning. this will be deprecated.
     '''
     file_regex_str : str
     read_method : Callable
@@ -28,16 +18,53 @@ class FileDescription:
     dataframe_post_process_kwarg : dict|None  = None
 
 class FileExecutor:
+    '''
+    Accept multiple files and reads some files you want.
+    
+    ## Examples:
+    >>> import pyplus.streamlit as stp
+    >>> 
+    >>> fe = stp.FileExecutor()
+    >>> fe += '^train.csv$',pd.read_csv
+    >>> fe += '^codebook.csv$',pd.read_csv
+    >>> dfs = fe()
+
+    '''
     behaviors : list[FileDescription] = []
-
-    def append_behavior(self,file_regex_str:str,read_method:Callable,post_method:Callable=None):
-        if post_method is None:
-            self.behaviors.append(FileDescription(file_regex_str,read_method))
-        else:
-            self.behaviors.append(FileDescription(file_regex_str,read_method,dataframe_post_process=post_method))
-        return self.behaviors
-
-    def execute_file_descriptions(self,show:bool=False,label:str='multifiles test')->dict[str,pd.DataFrame]:
+    
+    def __iadd__(self,other:tuple[str,Callable] | tuple[str,Callable,str|None,Callable,dict,dict] )->Self:
+        '''
+        add behavior
+        
+        Parameters
+        ----------
+        file_regex : str
+            regex for finding a file.
+        read_method : Callable
+            function how to convert a file to a dataframe
+        var_name : str|None
+            key name for file
+        dataframe_post_process : Callable
+            function how to do after converting to a dataframe
+        read_method_kward : dict|None
+            ??
+        dataframe_post_process_kwarg : dict|None
+            ??
+        
+        See Also
+        --------
+        (sa_description)
+        
+        Examples
+        --------
+        (descripton_of_line)
+        >>> (ex_code)
+        (result_of_code)
+        '''
+        self.behaviors.append(FileDescription(*other))
+        return self
+    
+    def __call__(self,show:bool=False,label:str='multifiles test'):
         '''
         Accept multiple files and reads some files you want.
         ## Parameters:
@@ -57,7 +84,60 @@ class FileExecutor:
         >>> 
         >>> dfs
         '''
-        
+        input_df={}
+        multi_files=st.file_uploader(label,accept_multiple_files=True)
+        for file in multi_files:
+            for behavior in self.behaviors:
+                if show:
+                    st.write(re.compile(behavior.file_regex_str))
+                    st.write(file.name)
+
+                if re.compile(normalize("NFC",behavior.file_regex_str)).match(normalize("NFC",file.name)) is not None:
+                    if behavior.var_name:
+                        input_key = behavior.var_name
+                    else:
+                        input_key = "_".join(file.name.split(".")[:-1])
+
+                    if show:
+                        st.write(f"Assuming {file.name} is a {input_key}")
+                    
+                    try:
+                        _temp_df = behavior.read_method(file,**behavior.read_method_kwarg)
+                    except Exception as e1:
+                        st.error(e1)
+                        try:
+                            _temp_df = behavior.read_method(file)
+                        except Exception as e:
+                            st.error(e)
+                    
+                    try:
+                        input_df[input_key] = behavior.dataframe_post_process(_temp_df,**behavior.dataframe_post_process_kwarg)
+                    except:
+                        try:
+                            input_df[input_key] = behavior.dataframe_post_process(_temp_df)
+                        except Exception as e:
+                            input_df[input_key] = _temp_df
+                            st.error(e)
+
+        return input_df
+
+
+    def append_behavior(self,file_regex_str:str,read_method:Callable,post_method:Callable=None):
+        '''
+        !!will be deprecated use += instead
+        '''
+        print(f'append_behavior will be deprecated use += instead')
+        if post_method is None:
+            self.behaviors.append(FileDescription(file_regex_str,read_method))
+        else:
+            self.behaviors.append(FileDescription(file_regex_str,read_method,dataframe_post_process=post_method))
+        return self.behaviors
+
+    def execute_file_descriptions(self,show:bool=False,label:str='multifiles test')->dict[str,pd.DataFrame]:
+        '''
+        !!will be deprecated use FileDescription(show,label) instead
+        '''
+        print(f'execute_file_descriptions will be deprecated use FileDescription(show,label) instead')
         input_df={}
         multi_files=st.file_uploader(label,accept_multiple_files=True)
         for file in multi_files:
