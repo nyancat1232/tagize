fb2448561408a963a37af3d463a1fe4462059cfe
Lua Jeon <nyancat2323.1232@gmail.com>
2024-03-26 11:13:43 +0900

    new

--- builtin/datetime_plus.py
+++ builtin/datetime_plus.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime,timezone
 
 def assume_year(date_given):
-    now_date = datetime.utcnow()
+    now_date = datetime.now(tz=timezone.utc)
     year = now_date.year
     comp_date = datetime(year,date_given.month,date_given.day)
     if comp_date > now_date:
